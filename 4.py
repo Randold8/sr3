1b6a5cba8e6e418ebe563c93854068e830dbc031
Randold8 <84187343+Randold8@users.noreply.github.com>
2022-10-16 19:58:09 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
 years = int(input("Сколько лет смотрим на экспонаты? "))
 print("Будет просмотрено " + str(years*365*96) + " экспонатов")
 number_exp = int(input("Сколько экспонатов нужно осмотреть? "))
-print("На просмотр уйдет " + str(number_exp/96/365) + " лет, " + str(number_exp/96) + " дней, " + str(number_exp*5) + " минут.")
+print("На просмотр уйдет " + str(number_exp/96/365) + " лет, или " + str(number_exp/96) + " дней, или " + str(number_exp*5) + " минут.")
